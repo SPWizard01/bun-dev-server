dce2909b9d802a060d63188f03e2335cdcc2a6a1
SPWizard01 <the.mtrix@gmail.com>
2024-11-23 14:28:23 +0200

    Update README and bunHmrPlugin for improved type handling and enhance package.json with new keywords and version bump

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
 # Bun Dev Server
 
-```
+```ts
 //devserver.ts
-import {startBunDevServer} from "bun-dev-server"
-import { file } from "bun"
+import { startBunDevServer } from "bun-dev-server";
+import { file } from "bun";
 
 startBunDevServer({
-    buildConfig: {
-        entrypoints: ["./src/app.ts"],
-        outdir: "dist",
-        splitting: true,
-        naming: {
-            asset: "assets/[name]-[hash].[ext]",
-            chunk: "chunks/[name]-[hash].[ext]",
-        }
+  buildConfig: {
+    entrypoints: ["./src/app.ts"],
+    //...
+    outdir: "dist",
+    splitting: true,
+    naming: {
+      asset: "assets/[name]-[hash].[ext]",
+      chunk: "chunks/[name]-[hash].[ext]",
     },
-    tls: {
-        cert: file("./serve_cert.pem"),
-        key: file("./serve_key.pem"),
-    },
-    writeManifest: true,
-    cleanServePath: true,
-    port: 4567,
-    enableTypeScriptWatch: true,
-    watchDir: "./src",
-})
+  },
+  tls: {
+    cert: file("./serve_cert.pem"),
+    key: file("./serve_key.pem"),
+  },
+  writeManifest: true,
+  cleanServePath: true,
+  port: 4567,
+  enableTypeScriptWatch: true,
+  watchDir: "./src",
+});
 ```
 
 ```
--- bunHmrPlugin.ts
+++ bunHmrPlugin.ts
@@ -1,4 +1,4 @@
-import { type BunPlugin } from "bun";
+import type { BunPlugin, Loader } from "bun";
 import { bunHotReload } from "./bunClientHmr";
 import { readFile } from "fs/promises";
 import { type BunDevServerSocketConfig } from "./bunServeConfig";
@@ -10,11 +10,16 @@ export function getBunHMRPlugin(config: BunDevServerSocketConfig) {
             let hmrAdded = false;
             build.onLoad({ filter: /\.m?tsx?/ }, async (args) => {
                 const contents = await readFile(args.path, { encoding: "utf-8" });
+                const isTSx = /\.m?tsx$/.test(args.path);
+                const isJSx = /\.m?jsx$/.test(args.path);
+                const isJS = /\.m?js$/.test(args.path);
+                const isTS = /\.m?ts$/.test(args.path);
+                const loader: Loader = isTSx ? "tsx" : isJSx ? "jsx" : isTS ? "ts" : isJS ? "js" : "text";
                 if (!hmrAdded) {
                     hmrAdded = true;
-                    return { contents: `import "bun-hot-reload"\n` + contents, loader: "ts" };
+                    return { contents: `import "bun-hot-reload"\n` + contents, loader };
                 }
-                return { contents, loader: "ts" };
+                return { contents, loader };
             });
             build.onLoad({ filter: /./, namespace: "bun-hot-reload" }, async (args) => {
 
